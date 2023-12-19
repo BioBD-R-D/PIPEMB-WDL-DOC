6be48645e0b92ae35f17b5f7aac163d8ceb04a4e
emolina85 <60270884+emolina85@users.noreply.github.com>
2023-12-19 15:16:56 -0300

    uploading the doc files

---
+++ doc/tutorial/data/getting_data_tutorial.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+wget https://storage.googleapis.com/gatk-test-data/wgs_fastq/NA12878_20k/H06HDADXX130110.1.ATCACGAT.20k_reads_1.fastq
+wget https://storage.googleapis.com/gatk-test-data/wgs_fastq/NA12878_20k/H06HDADXX130110.1.ATCACGAT.20k_reads_2.fastq
+wget https://storage.googleapis.com/gatk-test-data/wgs_fastq/NA12878_20k/H06HDADXX130110.2.ATCACGAT.20k_reads_1.fastq
+wget https://storage.googleapis.com/gatk-test-data/wgs_fastq/NA12878_20k/H06HDADXX130110.2.ATCACGAT.20k_reads_2.fastq
+wget https://storage.googleapis.com/gatk-test-data/wgs_fastq/NA12878_20k/H06JUADXX130110.1.ATCACGAT.20k_reads_1.fastq
+wget https://storage.googleapis.com/gatk-test-data/wgs_fastq/NA12878_20k/H06JUADXX130110.1.ATCACGAT.20k_reads_2.fastq
